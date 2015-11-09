b4201dd161866b5daf0f1e9819c63a7d3b9d0ae0
mrpib909 <Ryan@rhickey-router.clients.bradley.edu>
2015-11-09 13:36:49 -0600

    this is also a test

--- wiki.php
+++ wiki.php
@@ -19,7 +19,7 @@ if(isset($_GET['content'])){
  $content = $_GET['content'];
     file_put_contents('wiki.txt',$content);
 }
-$safe_content = htmlentities($content);
+$safe_content = htmlentities('<h1>'$content'</h1>');
 echo $safe_content;
 
 ?>
