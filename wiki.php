be802d5053874f861a6ccdcb72c399a37fafe6d6
mrpib909 <Ryan@Ryans-MacBook-Pro.local>
2015-11-08 21:05:43 -0600

    added part two of instructions

--- wiki.php
+++ wiki.php
@@ -6,6 +6,10 @@ if(file_exists('wiki.txt')){
 } else{
  $content = '(no content)';   
 }
+if(isset($_GET['content'])){
+ $content = $_GET['content'];
+    file_put_contents('wiki.txt',$content);
+}
 $safe_content = htmlentities($content);
 echo $safe_content;
 
